64c06f41d7e8187fac5ac25b5745ec1dae4bc95e
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2022-05-22 15:55:10 +0900

    Update 12.pas

--- 12.pas
+++ 12.pas
@@ -1,15 +1,15 @@
 var a: array[1..100, 1..100] of integer; //cоздаем матрицу a формата integer
-    i, j, N, M, summ :integer; //cоздаем переменные 
+    i, j, A, M, summ :integer; //cоздаем переменные 
 begin
-write('Введите N: '); 
-readln(N); // считываем переменную N
+write('Введите A: '); 
+readln(A); // считываем переменную N
 write('Введите M: '); 
 readln(M); // считываем переменную M
 summ := 0;
 //создаем матрицу и заполняем её
 for i:=1 to M do   //объявляем  цикл
   begin
-  for j:=1 to N do   //объявляем  цикл
+  for j:=1 to A do   //объявляем  цикл
   begin
   a[i,j] := random(0, 5);
    write(a[i,j], ' ');
