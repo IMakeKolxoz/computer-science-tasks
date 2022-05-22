23b958c20fd5548c6a7cc133be12e4cd70c6f0a6
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2022-05-22 16:46:18 +0900

    Create 14.pas

---
+++ 14.pas
@@ -0,0 +1,23 @@
+var a: array[1..100, 1..100] of integer; //cоздаем матрицу a формата integer
+    i, j, M :integer; //cоздаем переменные 
+begin
+write('Введите M: '); 
+readln(M); // считываем переменную M
+//создаем матрицу и заполняем её
+for i:=1 to M do   //объявляем  цикл
+  begin
+  for j:=1 to M do   //объявляем  цикл
+  begin
+    a[i,j] := random(100);
+  end;
+end; 
+for i:=1 to M do   //объявляем  цикл
+  begin
+    for j:=1 to M do   //объявляем  цикл
+      begin
+      if ((M-i+1) > j) then a[i,j] := 0;
+      write(a[i,j]: 3, ' ')
+      end;
+      writeln();
+end; 
+end.
