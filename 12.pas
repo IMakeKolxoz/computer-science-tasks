c3a33de0ec633ca0a89d49efa50d97584b82743a
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2022-05-22 15:49:25 +0900

    Create 12.pas

---
+++ 12.pas
@@ -0,0 +1,22 @@
+var a: array[1..100, 1..100] of integer; //cоздаем матрицу a формата integer
+    i, j, N, M, summ :integer; //cоздаем переменные 
+begin
+write('Введите N: '); 
+readln(N); // считываем переменную N
+write('Введите M: '); 
+readln(M); // считываем переменную M
+summ := 0;
+//создаем матрицу и заполняем её
+for i:=1 to M do   //объявляем  цикл
+  begin
+  for j:=1 to N do   //объявляем  цикл
+  begin
+  a[i,j] := random(0, 5);
+   write(a[i,j], ' ');
+   if (M+1-i) = j then 
+    summ := summ + a[i,j];
+  end;
+  writeln();
+end;  
+writeln('Сумма побочной диагонали диагонали: ', summ);
+end.
