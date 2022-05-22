f565660fba0c5be0a3095257c6547fe1c701dba2
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2022-05-22 15:27:18 +0900

    Update 8.pas

--- 8.pas
+++ 8.pas
@@ -5,18 +5,25 @@ write('Введите N: ');
 readln(N); // считываем переменную N
 write('Введите M: '); 
 readln(M); // считываем переменную M
-
 summ := 0;
+//создаем матрицу и заполняем её
 for i:=1 to N do   //объявляем  цикла
   begin
   for j:=1 to M do   //объявляем  цикла
   begin
   a[i,j] := random(5);
    write(a[i,j], ' ');
-   summ := summ + a[i,j];
    end;
    writeln();
- writeln('сумма элементов ',i,' строки: ', summ);
+end;  
+//подсчет всех элементов
+for i:=1 to N do   //объявляем  цикл для подсчета
+  begin
+  for j:=1 to M do   //объявляем  цикла
+  begin
+    summ := summ + a[i,j];
+   end;
+   writeln('сумма элементов ',i,' строки: ', summ);
  summ := 0;
 end;
 end.
