744b5aa3ab833ca24f3ca73ec778781cf1ccf737
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2022-05-26 20:37:52 +0900

    Create 23.pas

---
+++ 23.pas
@@ -0,0 +1,25 @@
+var a: array[1..100, 1..100] of integer; //cоздаем матрицу a формата integer
+    i, j, M,b  :integer; //cоздаем переменные 
+begin
+write('Введите M: '); 
+readln(M); // считываем переменную M
+//создаем матрицу и заполняем её
+for i:=1 to M do   //объявляем  цикл
+  begin
+  for j:=1 to M do   //объявляем  цикл
+  begin
+    a[i,j] := 0;
+  end;
+end; 
+for i:=1 to M do   //объявляем  цикл0
+  begin
+  b := 1;
+    for j:=1 to M do   //объявляем  цикл
+      begin
+      if ((M-i+1) = j) or ((i) = j) then a[i,j] := (b);
+      b := b+1;
+      write(a[i,j]: 3, ' ')
+      end;
+      writeln();
+end;
+end.
